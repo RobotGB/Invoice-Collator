7534c709707b076bf6f840b49c70240fb4e0cace
phil <phil@brainlabsdigital.com>
2017-07-19 15:11:26 +0100

    minor changes

--- bin/install.bat
+++ bin/install.bat
@@ -1,10 +1,10 @@
 @echo off
-echo "Before coninuing please ensure you have followed the steps on the wiki:"
-echo "\t1.Install XAMPP"
-echo "\t2.Add PHP to your system environment path"
+echo Before coninuing please ensure you have followed the steps on the wiki:
+echo     1.Install XAMPP
+echo     2.Add PHP to your system environment path
 pause
-echo "Installing appropriate libraries"
+echo Installing appropriate libraries
 cd ..
 composer install
-echo "Install complete"
+echo Install complete
 pause
