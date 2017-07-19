ed992a82d59f4229cae86c24374ac1caa32d5592
Phil <phil@brainlabsdigital.com>
2017-07-19 12:17:13 +0100

    added further instructions to install

--- bin/install.bat
+++ bin/install.bat
@@ -1,4 +1,8 @@
 @echo off
+echo "Before coninuing please ensure you have followed the steps on the wiki:"
+echo "\t1.Install XAMPP"
+echo "\t2.Add PHP to your system environment path"
+pause
 echo "Installing appropriate libraries"
 cd ..
 composer install
